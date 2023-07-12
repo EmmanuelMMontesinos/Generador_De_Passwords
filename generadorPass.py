4839a23b4248b802cedbbbc9e9e8cfdbb92bb648
Emmanuel M <emmanuelmmontesinos@gmail.com>
2023-07-12 11:33:46 +0200

    1.0.0.0

--- generadorPass.py
+++ generadorPass.py
@@ -52,7 +52,7 @@ def generador(numero, longitud):
     for num in range(numero):
         password = ""
         fpassword = ""
-        for log in range(longitud + 1):
+        for log in range(longitud):
             if log == longitud - 2:
                 password = password + choice(ESPECIALES)
                 password = password + choice(NUMEROS)
